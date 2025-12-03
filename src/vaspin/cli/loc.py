6452aaf03cea0e95ccbdbdcc1b93b69008045951
Xin Zhao <zx0@mail.ustc.edu.cn>
2025-12-03 21:32:41 +0800

    Feat(cli:loc): Make --atom in `loc` accept the index input

--- src/vaspin/cli/loc.py
+++ src/vaspin/cli/loc.py
@@ -8,7 +8,7 @@ from vaspin import Poscar, PosData
 from vaspin.types.array import FloatArray
 
 
-def get_defect_coor(pos, defect_site: str | list) -> FloatArray:
+def get_defect_coor(pos, defect_site: str | int | list) -> FloatArray:
     """Get the defect coordinates from the original structure.
 
     Args:
@@ -18,18 +18,18 @@ def get_defect_coor(pos, defect_site: str | list) -> FloatArray:
     Returns:
         defect_coor: the defect coordinates in fractional coordinates
     """
-    if not isinstance(defect_site, str) and not isinstance(defect_site, list):
+    if (
+        not isinstance(defect_site, str)
+        and not isinstance(defect_site, list)
+        and not isinstance(defect_site, int)
+    ):
         raise TypeError("defect_site should be a symbol or a list of coordinates")
 
     if isinstance(defect_site, str):
-        if defect_site not in pos.atoms:
-            raise ValueError(f"{defect_site} is not in the structure")
-        if np.sum(pos.atoms == defect_site) > 1:
-            raise ValueError(
-                f"{defect_site} is not unique in the structure, please use coordinates"
-            )
+        defect_coor = _get_defect_coor_from_symbol(pos, defect_site)
 
-        defect_coor = pos.coor_frac[pos.atoms == defect_site]
+    elif isinstance(defect_site, int):
+        defect_coor = _get_defect_coor_from_index(pos, defect_site)
 
     elif isinstance(defect_site, list):
         if len(defect_site) != 3:
@@ -39,6 +39,45 @@ def get_defect_coor(pos, defect_site: str | list) -> FloatArray:
 
         defect_coor = np.array(defect_site)
 
+    return defect_coor
+
+
+def _get_defect_coor_from_symbol(pos: Poscar, symbol: str) -> FloatArray:
+    """Get the defect coordinates from the original structure by symbol.
+
+    Args:
+        pos: the original structure contains the defect
+        symbol: the defect symbol
+
+    Returns:
+        defect_coor: the defect coordinates in fractional coordinates
+    """
+    if symbol not in pos.atoms:
+        raise ValueError(f"{symbol} is not in the structure")
+    if np.sum(pos.atoms == symbol) > 1:
+        raise ValueError(
+            f"{symbol} is not unique in the structure, please use coordinates"
+        )
+
+    defect_coor = pos.coor_frac[pos.atoms == symbol]
+
+    return defect_coor
+
+
+def _get_defect_coor_from_index(pos: Poscar, index: int) -> FloatArray:
+    """Get the defect coordinates from the original structure by index.
+
+    Args:
+        pos: the original structure contains the defect
+        index: the defect index, but starts from 1
+
+    Returns:
+        defect_coor: the defect coordinates in fractional coordinates
+    """
+    if index < 1 or index > len(pos.atoms):
+        raise ValueError(f"The index should be in the range of 1 to {len(pos.atoms)}")
+
+    defect_coor = pos.coor_frac[index - 1]
     return defect_coor
 
 
@@ -75,7 +114,9 @@ def get_local_stru(pos: Poscar, defect_coor: FloatArray, dmax: float) -> PosData
 def main():
     """The main logic"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--atom", help="input the dopant symbol")
+    parser.add_argument(
+        "-a", "--atom", help="input the dopant symbol, or index counting from 1"
+    )
     parser.add_argument(
         "-p",
         "--position",
